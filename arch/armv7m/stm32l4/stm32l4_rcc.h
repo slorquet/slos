edca9bff63c280d6939c4dd49ac512044ca92da2
Sebastien Lorquet <sebastien@lorquet.fr>
2016-11-24 21:14:39 +0100

    add config options to select l4 peripherals

--- arch/armv7m/stm32l4/stm32l4_rcc.h
+++ arch/armv7m/stm32l4/stm32l4_rcc.h
@@ -1,14 +1,11 @@
-#ifndef STM32_CLOCK
-#define STM32_CLOCK
+#ifndef _ARCH_ARMV7M_STM32L4_STM32L4_RCC_H_
+#define _ARCH_ARMV7M_STM32L4_STM32L4_RCC_H_
 
 #include <stdint.h>
+#include <bits/stm32l4_periphs.h>
+#include <bits/stm32l4_rcc.h>
 
-#define STM32_REG_RCC_BASE	0x40023800
-#define STM32_REG_RCC_AHB1ENR	(STM32_REG_RCC_BASE + 0x030)
-#define STM32_REG_RCC_APB1ENR	(STM32_REG_RCC_BASE + 0x040)
-#define STM32_REG_RCC_APB2ENR	(STM32_REG_RCC_BASE + 0x044)
+void stm32l4_clock_default(void);
+uint32_t stm32l4_clock_get(void);
 
-void stm32_clock_default(void);
-uint32_t stm32_clock_get(void);
-
-#endif //STM32_CLOCK
+#endif /* _ARCH_ARMV7M_STM32L4_STM32L4_RCC_H_*/
