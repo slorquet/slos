c678bbc57a3629f62188e13bcd62acdd4d013ff9
Sebastien Lorquet <sebastien@lorquet.fr>
2016-11-25 12:15:38 +0100

    Ensure std types have proper sizes, if failures happen we will use smarter things

--- include/stdint.h
+++ include/stdint.h
@@ -1,6 +1,8 @@
 #ifndef __STDINT__H__
 #define __STDINT__H__
 
+#include <assert.h>
+
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
 
@@ -12,5 +14,10 @@ typedef unsigned int uint32_t;
 
 typedef signed long long int int64_t;
 typedef unsigned long long int uint64_t;
+
+static_assert(sizeof( uint8_t)==1);
+static_assert(sizeof(uint16_t)==2);
+static_assert(sizeof(uint32_t)==4);
+static_assert(sizeof(uint64_t)==8);
 
 #endif /* __STDINT__H__ */
