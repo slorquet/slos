aa7ae8c1d3942b6cbeee3696f8776e39728b3609
Sebastien Lorquet <sebastien@lorquet.fr>
2016-12-08 16:13:19 +0100

    Add stdbool

---
+++ include/stdbool.h
@@ -0,0 +1,13 @@
+/* http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/stdbool.h.html */
+
+#ifndef __STDBOOL__H__
+#define __STDBOOL__H__
+
+#define true  1
+#define false 0
+#define bool  _Bool
+
+#define __bool_true_false_are_defined 1
+
+#endif /* __STDBOOL__H__ */
+
