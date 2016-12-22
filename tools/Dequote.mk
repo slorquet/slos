47f287797954b3b70bbb04cabab8a5598b52da69
Sebastien Lorquet <sebastien@lorquet.fr>
2016-12-22 18:08:58 +0100

    Remove more quotes

--- tools/Dequote.mk
+++ tools/Dequote.mk
@@ -1,2 +1,7 @@
+#the string values in the .config files are quotes
+#however make variables do not need this.
+#this script removes the string quotes on selected vars
 CONFIG_ARCH_CHIP_FAMILY:=$(patsubst "%",%,$(CONFIG_ARCH_CHIP_FAMILY))
 CONFIG_ARCH:=$(patsubst "%",%,$(CONFIG_ARCH))
+CONFIG_BOARD:=$(patsubst "%",%,$(CONFIG_BOARD))
+CONFIG_BOARD_LDSCRIPT:=$(patsubst "%",%,$(CONFIG_BOARD_LDSCRIPT))
