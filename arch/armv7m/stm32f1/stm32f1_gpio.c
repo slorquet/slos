3725bf206f61f45117dc04a81ac3322d8d7a3218
Sebastien Lorquet <sebastien@lorquet.fr>
2016-12-23 12:48:14 +0100

    let gpios enable themselves, like uarts remove initial rcc reg settings, this will be done on demand.

--- arch/armv7m/stm32f1/stm32f1_gpio.c
+++ arch/armv7m/stm32f1/stm32f1_gpio.c
@@ -5,48 +5,80 @@
 
 #include "bits/stm32f1_periphs.h"
 #include "bits/stm32f1_gpio.h"
+#include "bits/stm32f1_rcc.h"
 #include "stm32f1_gpio.h"
 
-static const uint32_t g_stm32f1_gpioaddr[] =
+/*==============================================================================
+ * Definitions
+ *==============================================================================
+ */
+
+/*==============================================================================
+ * Types
+ *==============================================================================
+ */
+
+struct stm32f1_gpio_s {
+    uint32_t base;
+    uint32_t rccreg;
+    uint32_t rccbit;
+};
+
+/*==============================================================================
+ * Function Prototypes
+ *==============================================================================
+ */
+
+/*==============================================================================
+ * Variables and constants
+ *==============================================================================
+ */
+
+static const struct stm32f1_gpio_s g_stm32f1_gpios[] =
 {
 #ifdef CONFIG_STM32F1_GPIOA
-  STM32F1_REGBASE_GPIOA,
+  {STM32F1_REGBASE_GPIOA,STM32F1_RCC_APB2ENR, RCC_APB2ENR_IOPAEN},
 #else
-  0,
+  {0,0,0},
 #endif
 #ifdef CONFIG_STM32F1_GPIOB
-  STM32F1_REGBASE_GPIOB,
+  {STM32F1_REGBASE_GPIOB,STM32F1_RCC_APB2ENR, RCC_APB2ENR_IOPBEN},
 #else
-  0,
+  {0,0,0},
 #endif
 #ifdef CONFIG_STM32F1_GPIOC
-  STM32F1_REGBASE_GPIOC,
+  {STM32F1_REGBASE_GPIOC,STM32F1_RCC_APB2ENR, RCC_APB2ENR_IOPCEN},
 #else
-  0,
+  {0,0,0},
 #endif
 #ifdef CONFIG_STM32F1_GPIOD
-  STM32F1_REGBASE_GPIOD,
+  {STM32F1_REGBASE_GPIOD,STM32F1_RCC_APB2ENR, RCC_APB2ENR_IOPDEN},
 #else
-  0,
+  {0,0,0},
 #endif
 #ifdef CONFIG_STM32F1_GPIOE
-  STM32F1_REGBASE_GPIOE,
+  {STM32F1_REGBASE_GPIOE,STM32F1_RCC_APB2ENR, RCC_APB2ENR_IOPEEN},
 #else
-  0,
+  {0,0,0},
 #endif
 #ifdef CONFIG_STM32F1_GPIOF
-  STM32F1_REGBASE_GPIOF,
+  {STM32F1_REGBASE_GPIOF,STM32F1_RCC_APB2ENR, RCC_APB2ENR_IOPFEN},
 #else
-  0,
+  {0,0,0},
 #endif
 #ifdef CONFIG_STM32F1_GPIOG
-  STM32F1_REGBASE_GPIOG,
+  {STM32F1_REGBASE_GPIOG,STM32F1_RCC_APB2ENR, RCC_APB2ENR_IOPGEN},
 #else
-  0,
+  {0,0,0},
 #endif
 };
 
-#define STM32F1_PORTCOUNT (sizeof(g_stm32f1_gpioaddr)/sizeof(g_stm32f1_gpioaddr[0]))
+#define STM32F1_PORTCOUNT (sizeof(g_stm32f1_gpios)/sizeof(g_stm32f1_gpios[0]))
+
+/*==============================================================================
+ * Functions
+ *==============================================================================
+ */
 
 /*----------------------------------------------------------------------------*/
 static inline void stm32f1_gpio_putreg(const uint32_t base, uint32_t regoff, uint32_t value)
@@ -80,11 +112,23 @@ void stm32f1_gpio_init(uint32_t gpiodesc)
     {
       return;
     }
-  base = g_stm32f1_gpioaddr[port];
+  base = g_stm32f1_gpios[port].base;
   if (base==0)
     {
       return;
     }
+
+  /* Enable peripheral if not already done */
+  
+  val = getreg32(STM32F1_REGBASE_RCC + g_stm32f1_gpios[port].rccreg);
+  val |= g_stm32f1_gpios[port].rccbit;
+  putreg32(STM32F1_REGBASE_RCC + g_stm32f1_gpios[port].rccreg, val);
+  
+  /* Enable AFIO if not already done */
+  
+  val = getreg32(STM32F1_REGBASE_RCC + STM32F1_RCC_APB2ENR);
+  val |= RCC_APB2ENR_AFIOEN;
+  putreg32(STM32F1_REGBASE_RCC + STM32F1_RCC_APB2ENR, val);
 
   /* First of all, single bit changes */
 
@@ -201,6 +245,7 @@ void stm32f1_gpio_init(uint32_t gpiodesc)
 }
 
 /*----------------------------------------------------------------------------*/
+/*For performance purposes, gpio initialization is not tested. */
 void stm32f1_gpio_write(uint32_t gpio, int state)
 {
   uint32_t base,reg,val;
@@ -210,7 +255,7 @@ void stm32f1_gpio_write(uint32_t gpio, int state)
     {
       return;
     }
-  base = g_stm32f1_gpioaddr[port];
+  base = g_stm32f1_gpios[port].base;
   if (base==0)
     {
       return;
--- arch/armv7m/stm32f1/stm32f1_uart.c
+++ arch/armv7m/stm32f1/stm32f1_uart.c
@@ -5,17 +5,30 @@
 #include "armv7m.h"
 #include "bits/stm32f1_periphs.h"
 #include "bits/stm32f1_rcc.h"
+#include "bits/stm32f1_gpio.h"
 #include "bits/irq.h"
+#include "stm32f1_gpio.h"
+
+/*==============================================================================
+ * Definitions
+ *==============================================================================
+ */
+
+/*==============================================================================
+ * Types
+ *==============================================================================
+ */
 
 struct stm32f1_uartparams_s
 {
   uint32_t regbase;
   uint32_t rccreg;
   uint32_t rccbit;
-  uint32_t  remapreg;  /* IO remap register */
-  uint32_t  remapbit;  /* IO remap bit */
-  uint32_t  txpin;
-  uint32_t  rxpin;
+  uint32_t remapreg;   /* IO remap register  */
+  uint32_t remapmask;  /* IO remap bit mask  */
+  uint32_t remapbit;   /* IO remap bit value */
+  uint32_t txpin;
+  uint32_t rxpin;
   uint8_t  irq;    /* irq line */
 };
 
@@ -27,6 +40,11 @@ struct stm32f1_uart_s
   uint8_t                            drivemode; /* none, irq, dma */
 };
 
+/*==============================================================================
+ * Function Prototypes
+ *==============================================================================
+ */
+
 static int stm32f1_uart_init (struct uart_s *uart);
 static int stm32f1_uart_fini (struct uart_s *uart);
 static int stm32f1_uart_write(struct uart_s *uart, const uint8_t *buf, int len);
@@ -34,6 +52,11 @@ static int stm32f1_uart_flush(struct uart_s *uart);
 static int stm32f1_uart_avail(struct uart_s *uart);
 static int stm32f1_uart_read (struct uart_s *uart, uint8_t *buf, int len);
 static int stm32f1_uart_ioctl(struct uart_s *uart, int command, void* params);
+
+/*==============================================================================
+ * Variables and constants
+ *==============================================================================
+ */
 
 static const struct uart_ops_s g_stm32f1_uartops =
 {
@@ -54,15 +77,15 @@ static const struct stm32f1_uartparams_s g_stm32f1_usart1params =
   .rccbit     = RCC_APB2ENR_USART1EN,
   .irq        = STM32F1_IRQ_USART1,
   .remapreg   = STM32F1_AFIO_MAPR,
-  .remapmask  = AFIO_USART1_REMAP,
+  .remapmask  = AFIO_MAPR_USART1_REMAP,
 #ifdef CONFIG_STM32F1_USART1_REMAP
-  .remapbit   = AFIO_MAPR_USART1REMAP
-  .txpin      = GPIO_PORT_B | GPIO_PIN_6 | GPIO_MODE_ALT | GPIO_SPEED_MED | GPIO_TYPE_PP;
-  .rxpin      = GPIO_PORT_B | GPIO_PIN_7 | GPIO_MODE_IN  | GPIO_SPEED_MED | GPIO_PULL_UP;
+  .remapbit   = AFIO_MAPR_USART1_REMAP,
+  .txpin      = GPIO_PORT_B | GPIO_PIN_6 | GPIO_MODE_ALT | GPIO_SPEED_MED | GPIO_TYPE_PP,
+  .rxpin      = GPIO_PORT_B | GPIO_PIN_7 | GPIO_MODE_IN  | GPIO_SPEED_MED | GPIO_PULL_UP,
 #else
-  .remapbit   = 0
-  .txpin      = GPIO_PORT_A | GPIO_PIN_9  | GPIO_MODE_ALT | GPIO_SPEED_MED | GPIO_TYPE_PP;
-  .rxpin      = GPIO_PORT_A | GPIO_PIN_10 | GPIO_MODE_IN  | GPIO_SPEED_MED | GPIO_PULL_UP;
+  .remapbit   = 0,
+  .txpin      = GPIO_PORT_A | GPIO_PIN_9  | GPIO_MODE_ALT | GPIO_SPEED_MED | GPIO_TYPE_PP,
+  .rxpin      = GPIO_PORT_A | GPIO_PIN_10 | GPIO_MODE_IN  | GPIO_SPEED_MED | GPIO_PULL_UP,
 #endif
 };
 
@@ -89,6 +112,11 @@ static struct stm32f1_uart_s g_stm32f1_usart1 =
 };
 #endif
 
+/*==============================================================================
+ * Functions
+ *==============================================================================
+ */
+
 /*----------------------------------------------------------------------------*/
 /*setup baud rate and port params, enable uart*/
 static int stm32f1_uart_init(struct uart_s *uart)
@@ -98,9 +126,9 @@ static int stm32f1_uart_init(struct uart_s *uart)
   
   /* Power up device */
 
-  val  = getreg32(STM32F1_BASEREG_RCC + dev->params->rccreg;
+  val  = getreg32(STM32F1_REGBASE_RCC + dev->params->rccreg);
   val |= dev->params->rccbit;
-  putreg(STM32F1_REGBASE_RCC + dev->params->rccreg, val);
+  putreg32(STM32F1_REGBASE_RCC + dev->params->rccreg, val);
   
   /* Enable GPIOs and alternate settings */
 
@@ -124,6 +152,7 @@ static int stm32f1_uart_init(struct uart_s *uart)
 /*disable uart*/
 static int stm32f1_uart_fini (struct uart_s *uart)
 {
+  uint32_t val;
   struct stm32f1_uart_s *dev = (struct stm32f1_uart_s *)uart;
 
   /* Disable uart */
@@ -136,9 +165,9 @@ static int stm32f1_uart_fini (struct uart_s *uart)
 
   /* Power down device */
 
-  val  = getreg32(STM32F1_BASEREG_RCC + dev->params->rccreg;
+  val  = getreg32(STM32F1_REGBASE_RCC + dev->params->rccreg);
   val &= ~dev->params->rccbit;
-  putreg(STM32F1_REGBASE_RCC + dev->params->rccreg, val);
+  putreg32(STM32F1_REGBASE_RCC + dev->params->rccreg, val);
 
   return -ENOSYS;
 }
