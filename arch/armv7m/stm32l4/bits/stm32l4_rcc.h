7402d570b8421e6bdbcb74838f8d9bf0d589abb4
Sebastien Lorquet <sebastien@lorquet.fr>
2017-07-06 02:40:04 +0200

    split stm32l4 rcc definitions per model

--- arch/armv7m/stm32l4/bits/stm32l4_rcc.h
+++ arch/armv7m/stm32l4/bits/stm32l4_rcc.h
@@ -1,9 +1,12 @@
 #ifndef _ARCH_ARMV7M_STM32L4_BITS_STM32L4_RCC_H_
 #define _ARCH_ARMV7M_STM32L4_BITS_STM32L4_RCC_H_
 
-#if defined( ARCH_CHIPFAMILY_STM32L4X6 )
+#include "config.h"
+
+
+#if defined( CONFIG_ARCH_CHIPFAMILY_STM32L4X6 )
 #include "bits/stm32l4x6xx_rcc.h"
-#elif defined( ARCH_CHIPFAMILY_STM32L4X3 )
+#elif defined( CONFIG_ARCH_CHIPFAMILY_STM32L4X3 )
 #include "bits/stm32l4x3xx_rcc.h"
 #else
 #error undefined STM32L4 family
